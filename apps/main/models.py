ec04ea9ee75a4d169e57f97caf2493835d31539d
Luis Sucuc <luissucuco@gmail.com>
2021-02-10 16:08:55 -0600

    Bugs para mostrar informacion

--- apps/main/models.py
+++ apps/main/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Persona(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    id        = models.AutoField(primary_key=True)
+    name      = models.CharField(max_length=100)
     last_name = models.CharField(max_length=120)
-    email = models.EmailField(max_length=254)
+    email     = models.EmailField(max_length=254)
 
     def __str__(self):
+        #Por defecto muestra el nombre para cáda campo
         return self.name
     
 
--- apps/main/views.py
+++ apps/main/views.py
@@ -4,8 +4,6 @@ from .models import Persona
 
 def inicio(request):
     personas = Persona.objects.all()
-    print(personas)
-    context = {personas: personas}
+    context = { 'personas' : personas}
     return render(request, 'index.html', context)
 
-# Create your views here.
